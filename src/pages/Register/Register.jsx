9752d79817c8af4071cb048474b99f92ad18e964
unknown <vohongphat2892005@gmail.com>
2024-08-05 23:00:41 +0700

    done task design

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -50,12 +50,21 @@ const Register = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div
+      className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+    </div>
+    <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+   
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold mb-4 text-center">Register</h1>
+         <div className="max-w-md mx-auto">
+        <h1 className="text-2xl text-center font-semibold">Register</h1>
+        
         {message && <div className="text-green-500 text-center mb-4">{message}</div>}
         {error && <div className="text-red-500 text-center mb-4">{error}</div>}
         <form onSubmit={handleRegister} className="space-y-4">
+        <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
           <label className="block">
             <span className="block text-gray-700">Name:</span>
             <input
@@ -114,17 +123,22 @@ const Register = () => {
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
             >
               <option value="user">User</option>
-              <option value="admin">Admin</option>
+              <option hidden value="admin">Admin</option>
             </select>
           </label>
+          
           <button
             type="submit"
             className="w-full py-2 px-4 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Register
           </button>
+          </div>
         </form>
+        </div>
       </div>
+    </div>
+    </div>
     </div>
   );
 };
